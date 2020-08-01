052802b0c7d64e5c68314387a64051b1c35346c3
Kalki <ppranjalmishra@gmail.com>
2020-08-01 17:59:34 +0530

    Added candidate Form layout.

--- application/candidate.php
+++ application/candidate.php
@@ -11,8 +11,37 @@
 </head>
 
 <body>
+  <nav class="navbar border-bottom">
+    <div class="nav-brand">
+      <h5>Application Form</h5>
+    </div>
+    <div class="inline-flex">
+      <a href="#" class="btn btn-primary">Back to Application</a>
+    </div>
+  </nav>
+
   <div class="container-fluid">
-     
+    <div class="row">
+      <div class="col-3 p-0">
+        <div class="list-group">
+          <a href="#" class="list-group-item active">Candidate Details</a>
+          <a href="#" class="list-group-item">Upload Photo And Signature</a>
+          <a href="#" class="list-group-item">Academic Details</a>
+          <a href="#" class="list-group-item">NET / SLET / SET / GATE</a>
+          <a href="#" class="list-group-item">Upload Documents</a>
+          <a href="#" class="list-group-item">Research Degree</a>
+          <a href="#" class="list-group-item">Fellowship / Awards</a>
+          <a href="#" class="list-group-item">Employment Details</a>
+          <a href="#" class="list-group-item">Field Of Specialization</a>
+          <a href="#" class="list-group-item">Teaching, Learning & Evaluation related activities</a>
+          <a href="#" class="list-group-item">Research & Academic Contributions</a>
+          <a href="#" class="list-group-item">API score</a>
+          <a href="#" class="list-group-item">Other Details</a>
+          <a href="#" class="list-group-item">Declaration</a>
+        </div>
+      </div>
+      <div class="border col"></div>
+    </div>
   </div>
 </body>
 
